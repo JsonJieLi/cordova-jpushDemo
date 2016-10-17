52cfdd977366f3c9768e747c2eb45d19bb2ad175
jiejieLI <15800043287@163.com>
2016-10-17 10:26:56 +0800

    加入文件

---
+++ platforms/ios/HelloCordova/Plugins/cn.jpush.phonegap.JPushPlugin/JPushPlugin.h
@@ -0,0 +1,26 @@
+//
+//  PushTalkPlugin.h
+//  PushTalk
+//
+//  Created by zhangqinghe on 13-12-13.
+//
+//
+
+#import <Cordova/CDV.h>
+
+#define kJPushPlugReceiveNotificaiton @"JPushPluginRecieveNotification"
+
+@interface JPushPlugin : CDVPlugin{
+
+    NSString * deviceT;
+    
+}
+
+-(void)setTagsWithAlias:(CDVInvokedUrlCommand*)command;
+-(void)setTags:(CDVInvokedUrlCommand*)command;
+-(void)setAlias:(CDVInvokedUrlCommand*)command;
+-(void)getRegistrationID:(CDVInvokedUrlCommand*)command;
+-(void)startLogPageView:(CDVInvokedUrlCommand*)command;
+-(void)stopLogPageView:(CDVInvokedUrlCommand*)command;
+
+@end
